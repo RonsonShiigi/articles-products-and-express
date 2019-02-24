209577b2cf54bd462cf0db3d9647909c8ada4df9
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-02-24 07:09:07 -1000

    added article page to show up

--- db/articles.js
+++ db/articles.js
@@ -1,14 +1,40 @@
-const collection = [];
-
-function all() {
-  return collection;
+class DS_Articles {
+  constructor() {
+    this.storage = [];
+    this.idNum = 1;
+    this.preArticles();
+  }
+  preArticles() {
+    this.storage.push({
+      title: "The King is Here",
+      body: "Lebron James Dunks on YOU!",
+      author: "JR Smith",
+      urlTitle: "lebron"
+    });
+    this.idNum++;
+    this.storage.push({
+      title: "Rock Out the Right Way",
+      body: "Feel It",
+      author: "Jimmy Page",
+      urlTitle: "rock"
+    });
+    this.idNum++;
+  }
+  getAllArticles() {
+    console.log(this.storage);
+    return this.storage.slice();
+  }
+  getArticleByTitle(title) {
+    let result;
+    console.log("title", title);
+    this.storage.forEach(article => {
+      if (article.title === title) {
+        result = article;
+      }
+    });
+    console.log("result", result);
+    return result;
+  }
 }
 
-function add(params) {}
-
-module.exports = {
-  all: all,
-  add: add,
-  getByTitle: _getByTitle,
-  editByTitle: _editByTitle
-};
+module.exports = new DS_Articles();
--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var router = express.Router();
+const DS_Articles = require("../db/articles");
 
 //middleware that is specific to this router
 
 router.get("/", (req, res) => {
-  res.render("articles");
+  const artTitle = DS_Articles.getAllArticles();
+  res.render("articles", { artTitle });
+});
+
+router.get("/:title", (req, res) => {
+  const articleName = req.params.title.replace(/%20/g, "");
+  //   console.log("articleName", articleName);
+
+  const article = DS_Articles.getArticleByTitle(articleName);
+  console.log("article", article);
+  res.render("articlesSpec", article);
 });
 
 module.exports = router;
